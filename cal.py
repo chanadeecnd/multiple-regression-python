493abf362cef1c3211dc2685bf4520cc01f86ca6
chanadeecnd <chanadeehk4082@gmail.com>
2025-08-21 16:03:14 +0700

    rename variable

--- cal.py
+++ cal.py
@@ -1,17 +1,27 @@
 
-def calculate_vol_set(volt_out, freq):
-    indicator = 74.9390
-    b_vol = 1.5959
-    b_freq = -69.49817
+def calculate_required_voltage(desired_output_voltage, operating_frequency_mhz):
+    # Coefficients from MLR
+    intercept = 74.9390
+    coeff_set_voltage = 1.5959
+    coeff_frequency = -69.49817  # per MHz
 
+    # Solve for Set Voltage
+    required_set_voltage = (
+        desired_output_voltage 
+        - (coeff_frequency * operating_frequency_mhz) 
+        - intercept
+    ) / coeff_set_voltage
 
-    vol_set = (volt_out - (b_freq * freq) - indicator) / b_vol
-    print(f"freq: {freq}, volt_out: {volt_out}, vol_set: {vol_set}")
-    return vol_set
+    print(
+        f"Frequency: {operating_frequency_mhz} MHz, "
+        f"Desired Output Voltage: {desired_output_voltage} V, "
+        f"Required Set Voltage: {required_set_voltage:.2f}"
+    )
+    return required_set_voltage
 
-calculate_vol_set(60, 2)
-calculate_vol_set(100, 1.9)
-calculate_vol_set(100, 1.8)
-calculate_vol_set(100, 1.7)
-calculate_vol_set(100, 1.6)
+calculate_required_voltage(100, 2)
+calculate_required_voltage(100, 1.9)
+calculate_required_voltage(100, 1.8)
+calculate_required_voltage(100, 1.7)
+calculate_required_voltage(100, 1.6)
 
